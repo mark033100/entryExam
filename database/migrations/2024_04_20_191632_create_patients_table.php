d476c001b5dd6b1d1f9e314985f88c6fa1389deb
Mark <marktumbaga@gmail.com>
2024-04-21 03:22:48 +0800

    Patient Table columns

---
+++ database/migrations/2024_04_20_191632_create_patients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName');
+            $table->string('middleName');
+            $table->string('lastName');
+            $table->string('suffix');
+            $table->date('dateofBirth');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
